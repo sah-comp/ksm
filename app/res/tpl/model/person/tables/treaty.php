b9a4da7f084240609103f4a2d2d64eaf2dd6e531
sah-comp <info@sah-company.com>
2021-02-02 14:24:33 +0100

    Added tab treaty to addressbook edit form to close #51

--- app/res/tpl/model/person/tables/treaty.php
+++ app/res/tpl/model/person/tables/treaty.php
@@ -1,6 +1,46 @@
 <?php
 /**
- * List all treaty beans linked to this person (client)
+ * List all machines of this person (client)
  */
+Flight::render('script/datatable_config');
+// load our contracts from which the machines are taken
+$_treaties = R::find('treaty', "person_id = ? ORDER BY number", [$record->getId()]);
 ?>
-<p>Coming soon.</p>
+<table class="datatable">
+    <thead>
+        <tr>
+            <th><?php echo I18n::__('treaty_label_contracttype') ?></th>
+            <th><?php echo I18n::__('treaty_label_number') ?></th>
+            <th><?php echo I18n::__('treaty_label_startdate') ?></th>
+            <th><?php echo I18n::__('treaty_label_enddate') ?></th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($_treaties as $_treaty_id => $_treaty):
+    ?>
+        <tr>
+            <td
+                data-order="<?php echo htmlspecialchars($_treaty->contracttype->name) ?>">
+                <?php echo htmlspecialchars($_treaty->contracttype->name) ?>
+            </td>
+            <td
+                data-order="<?php echo htmlspecialchars($_treaty->number) ?>">
+                <a
+                    href="<?php echo Url::build('/admin/%s/edit/%d/', [$_treaty->getMeta('type'), $_treaty->getId()]) ?>"
+                    class="in-table">
+                    <?php echo htmlspecialchars($_treaty->number) ?>
+                </a>
+            </td>
+            <td
+                data-order="<?php echo htmlspecialchars($_treaty->startdate) ?>">
+                <?php echo htmlspecialchars($_treaty->localizedDate('startdate')) ?>
+            </td>
+            <td
+                data-order="<?php echo htmlspecialchars($_treaty->enddate) ?>">
+                <?php echo htmlspecialchars($_treaty->localizedDate('enddate')) ?>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
