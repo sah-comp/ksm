aefafaef956a926d68cb1b2cd098d9d2783d9a07
sah-comp <info@sah-company.com>
2022-03-30 17:31:11 +0200

    Added several toolbar options for quill editor

--- public/js/correspondence.js
+++ public/js/correspondence.js
@@ -4,13 +4,29 @@
  /* Ready, Set, Go. */
 
 $('body').ready(function() {
+    var toolbarOptions = [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean']                                         // remove formatting button
+    ];
     var options = {
         debug: 'info',
         modules: {
-            toolbar: [
-                [{ header: [1, 2, false] }],
-                ['bold', 'italic', 'underline'],
-            ]
+            toolbar: toolbarOptions
         },
         placeholder: '',
         readOnly: false,
